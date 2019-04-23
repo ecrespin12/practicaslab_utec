154bde454c1a4e081a977e8409ed77835d3ffdd4
Oscar Torres <40122963+JossueTorres@users.noreply.github.com>
2019-04-23 00:26:10 -0600

    Programar Practicas
    -Mostrar listado de practicas en interfaz tipo calendario.
    -Interfaz para ingresar o editar programacion por fecha, mes o ciclo.
    -Accion especial para borrar de forma avanzada

---
+++ application/controllers/admin/ControlLaboratorio.php
@@ -0,0 +1,121 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ControlLaboratorio extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		//verificar la session de usuario
+		//verificar la session de usuario
+		if (!$this->session->userdata("login")) {
+			redirect(base_url());
+		}
+	}
+
+	public function index()
+	{
+		$data['mes'] = (int)date('m');
+		$data['anio'] = (int)date('Y');
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/aside');
+		$this->load->view('layouts/nav');
+		$this->load->view('admin/controlLaboratorio', $data);
+		$this->load->view('layouts/footer');
+		$this->load->view('scripts/controlLab');
+	}
+
+	// public function changeMes($mes)
+	// {
+	// 	$a = $this->input->post('txtAnio');
+	// 	$data['mes'] = $mes;
+	// 	if ($a > 0) {
+	// 		$data['anio'] = $a;
+	// 	} else
+	// 		$data['anio'] = date('Y');
+
+	// 	$this->load->view('layouts/header');
+	// 	$this->load->view('layouts/aside');
+	// 	$this->load->view('layouts/nav');
+	// 	$this->load->view('admin/controlLaboratorio', $data);
+	// 	$this->load->view('layouts/footer');
+	// 	$this->load->view('scripts/controlLab');
+	// }
+
+	public function filtrar()
+	{
+		if ($this->input->post()) {
+			$data['mes'] = $this->input->post('ddlMes');
+			$data['anio'] = $this->input->post('txtAnio');
+		} else {
+			$data['mes'] = date('m');
+			$data['anio'] = date('Y');
+		}
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/aside');
+		$this->load->view('layouts/nav');
+		$this->load->view('admin/controlLaboratorio', $data);
+		$this->load->view('layouts/footer');
+		$this->load->view('scripts/controlLab');
+	}
+
+	public function getListaPrac()
+	{
+		$f = $this->input->post('fecha');
+		$practicas = array(
+			array(
+				'cod' => '1',
+				'lab' => 'LAB-1',
+				'fec' => '21/04/2019',
+				'ini' => '10:00',
+				'fin' => '12:00',
+				'cup' => 30
+			),
+			array(
+				'cod' => '2',
+				'lab' => 'LAB-1',
+				'fec' => '20/04/2019',
+				'ini' => '13:00',
+				'fin' => '16:00',
+				'cup' => 25
+			),
+			array(
+				'cod' => '3',
+				'lab' => 'LAB-2',
+				'fec' => '21/04/2019',
+				'ini' => '10:00',
+				'fin' => '12:00',
+				'cup' => 15
+			),
+			array(
+				'cod' => '4',
+				'lab' => 'LAB-2',
+				'fec' => '20/04/2019',
+				'ini' => '13:00',
+				'fin' => '16:00',
+				'cup' => 25
+			),
+			array(
+				'cod' => '5',
+				'lab' => 'LAB-3',
+				'fec' => '21/04/2019',
+				'ini' => '10:00',
+				'fin' => '12:00',
+				'cup' => 20
+			),
+			array(
+				'cod' => '6',
+				'lab' => 'LAB-3',
+				'fec' => '20/04/2019',
+				'ini' => '13:00',
+				'fin' => '16:00',
+				'cup' => 30
+			)
+		);
+		$ret = array_filter($practicas, function ($var) use ($f) {
+			if ($var['fec'] == $f) return $f;
+		});
+		header('Content-Type: application/json');
+		echo json_encode($ret);
+	}
+}
--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -1,9 +1,7 @@
    <?php
           $user=  $this->session->userdata("usuario");
-   ?>
-   
+   ?>   
    <!-- sidebar menu -->
-
    <!-- menu estudiante -->
    <?php if($user=="estudiante"):?>
    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
@@ -11,8 +9,7 @@
              <ul class="nav side-menu">
                <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Inicio</a></li>
                <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Practicas</a></li>
-               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Cerrar Sesión</a></li>
-            
+               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Cerrar Sesión</a></li>            
              </ul>
            </div>
          </div>
@@ -23,14 +20,14 @@
              <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Inicio</a></li>
              <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Control Laboratorio</a></li>
                </li>
-               <li class="current-page"><a href="#"><i class="fa fa-calendar"></i> Programar Practica<span class="fa fa-chevron-down"></span></a>
+               <li><a href="<?php echo base_url("/Admin/controlLaboratorio");?>"><i class="fa fa-calendar"></i>&nbsp;Programar Practica</a></li>
+               <!-- <li class="current-page"><a href="#"><i class="fa fa-calendar"></i> Programar Practica<span class="fa fa-chevron-down"></span></a>
                  <ul class="nav child_menu">
                    <li><a href="#">Dia</a></li>
                    <li><a href="#">Mes</a></li>
                    <li><a href="#">Ciclo</a></li>
-
                  </ul>
-               </li>
+               </li> -->
                <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Informes</a></li>
                <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Cerrar Sesion</a></li>     
              </ul>
@@ -43,23 +40,20 @@
              <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Inicio</a></li>
              <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Control Laboratorio</a></li>
                </li>
-               <li><a href="#"><i class="fa fa-calendar"></i> Programar Practica<span class="fa fa-chevron-down"></span></a>
+               <li><a href="<?php echo base_url("/Admin/controlLaboratorio");?>"><i class="fa fa-calendar"></i>&nbsp;Programar Practica</a></li>
+               <!-- <li><a href="#"><i class="fa fa-calendar"></i> Programar Practica<span class="fa fa-chevron-down"></span></a>
                  <ul class="nav child_menu">
                    <li><a href="#">Dia</a></li>
                    <li><a href="#">Mes</a></li>
                    <li><a href="#">Ciclo</a></li>
-
                  </ul>
-               </li>
+               </li> -->
                <li><a href="#"><i class="fa fa-calendar"></i> Encargado Laboratorio<span class="fa fa-chevron-down"></span></a>
                  <ul class="nav child_menu">
                    <li><a href="#">Nuevos Encargados</a></li>
-                   <li><a href="#">Asignar Laboratorios</a></li>
-              
-
+                   <li><a href="#">Asignar Laboratorios</a></li>              
                  </ul>
-               </li>
-             
+               </li>             
                <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Informes</a></li>
                <li><a href="#"><i class="fa fa-calendar"></i> Configuración<span class="fa fa-chevron-down"></span></a>
                  <ul class="nav child_menu">
@@ -76,7 +70,5 @@
          </div>
   <?php endif ?>;     
     <!-- sidebar menu -->
-
-
   </div>
 </div>
