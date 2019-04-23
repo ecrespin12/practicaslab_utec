5c616679f472df8ac47d1c9dd4b81941c7749070
Mauricio Rivera <49140795+Rivera-EM@users.noreply.github.com>
2019-04-22 21:58:51 -0600

    Se modifico form de editar
    Se agrego campos para cambiar de contraseña

--- application/views/admin/usuarios.php
+++ application/views/admin/usuarios.php
@@ -52,7 +52,7 @@
                             <td>
                             <div class="botonesList">
                   
-                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio" ><span class="fa fa-pencil"></span></button> 
+                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio-editar" ><span class="fa fa-pencil"></span></button> 
                               <button class="btn btn-sm btn-danger" type="button"><span class="fa fa-remove"></span></button> 
                             </div>              
                             </td>
@@ -70,7 +70,7 @@
                             <td>
                             <div class="botonesList">
                   
-                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio" ><span class="fa fa-pencil"></span></button> 
+                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio-editar" ><span class="fa fa-pencil"></span></button> 
                               <button class="btn btn-sm btn-danger" type="button"><span class="fa fa-remove"></span></button> 
                             </div>              
                             </td>
@@ -88,7 +88,7 @@
                             <td>
                             <div class="botonesList">
                   
-                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio" ><span class="fa fa-pencil"></span></button> 
+                              <button class="btn btn-sm btn-warning" type="button" data-toggle="modal" data-target="#modal-edificio-editar" ><span class="fa fa-pencil"></span></button> 
                               <button class="btn btn-sm btn-danger" type="button"><span class="fa fa-remove"></span></button> 
                             </div>              
                             </td>
@@ -201,4 +201,101 @@
     </div>
     <!-- /.modal -->
 
+        <!-- modal form -->
+        <div class="modal fade" id="modal-edificio-editar">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">FORMULARIO DE USUARIO</h4>
+                </div>
+                <div class="modal-body">            
+                <form name="frm" action="" method="POST">
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>NOMBRE</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="nombre" placeholder="Ingresar Nombre" required="">
+                        </div>
+
+                      </div>
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>APELLIDOS</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="apellidos" placeholder="Ingresar Apellidos" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>CORREO</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="correo" placeholder="Ingresar Correo" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>TIPO DE USUARIO</label><br>
+                          <input type="radio" name="tipousuario" value="SuperAdmin" checked> Super Administrador<br>
+                          <input type="radio" name="tipousuario" value="Admin"> Administrador<br>
+                          
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>TELEFONO</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="telefono" placeholder="Ingresar Telefono" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>CELULAR</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="celular" placeholder="Ingresar Celular" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>CAMBIAR CONTRASEÑA</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="celular" placeholder="Ingresar Contraseña nueva" required="">
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="celular" placeholder="Confirme Contraseña " required="">
+                        </div>
+                      </div>
+
+                      
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>Estado </label><br/>
+                          <label>
+                            <input type="checkbox" class="js-switch" checked name="estado" /> Activo
+                          </label>
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                            <div class="ln_solid"></div>
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                          <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                           
+                            
+                          </div>
+                      </div>
+                    </form>                                     
+                </div>
+
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 
