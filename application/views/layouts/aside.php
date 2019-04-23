dd97181a995cb894089f6732b21e72238fb887e6
Ado595 <49847204+Ado595@users.noreply.github.com>
2019-04-23 11:39:15 -0600

    Update aside.php

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -9,9 +9,9 @@
    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
            <div class="menu_section">
              <ul class="nav side-menu">
-               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Inicio</a></li>
-               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Practicas</a></li>
-               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Cerrar Sesión</a></li>
+              <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-home"></i> Inicio</a></li>
+               <li><a href="<?php echo base_url();?>estudiante/Practica"><i class="fa fa-calendar"></i> Practicas</a></li>
+               <li><a href="<?php echo base_url();?>Dashboard"><i class="fa fa-sign-out"></i> Cerrar Sesión</a></li>
             
              </ul>
            </div>
