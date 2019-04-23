51f805cdfde1e9c408042aa072495b9fc2314f39
williamdiaz798 <williamdiaz798@users.noreply.github.com>
2019-04-23 01:03:36 -0600

    Control Lab
    Se añadio la vista de laboratorios y el añadir laboratorio y el editar

---
+++ application/views/laboratorio/Laboratorios/laboratorios.php
@@ -0,0 +1,241 @@
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+              <h2 class="fuentetitulo">LABORATORIO 1</h2>
+              <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            
+            
+            <hr>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="">
+                  <div class="row">
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-01</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-02</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-03</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-04</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-05</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-06</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-07</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-08</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-09</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-10</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                    <br>
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-11</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-12</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-13</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-14</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-15</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-16</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-17</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-18</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-19</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-20</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                    <br>
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-21</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-22</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-23</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-24</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-25</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-26</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-27</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-28</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-29</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-30</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                    <br>
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-31</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-32</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-33</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-34</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-35</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-36</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-37</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-38</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-39</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-40</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                    <br>
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-51</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-52</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-53</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-54</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-55</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-56</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-57</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-58</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-59</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-60</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                    <br>
+                    <div class="col s1">
+                      <label class = 'btn my-black'   >PC-61</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-62</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-63</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-64</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-65</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-66</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-67</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-68</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-69</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                      <label class = 'btn my-black'   >PC-70</label>
+                      <img src="https://pngimage.net/wp-content/uploads/2018/06/icone-informatique-png-1.png" alt="" width="60" height="60">
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- /page content -->
+
+
+        <!-- modal form -->
+        <div class="modal fade" id="modal-edificio">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">FORMULARIO DE EDIFICIO</h4>
+                </div>
+                <div class="modal-body">            
+                <form name="frm" action="" method="POST">
+                      <div class="form-row">
+
+                        <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                          <label>Codigo</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="codigo" placeholder="Ingresar Codigo" required="">
+                        </div>
+
+                      </div>
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>Nombre</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="nombre" placeholder="Ingresar Nombre" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>Descripcion</label>
+                          <input type="text" class="form-control col-md-7 col-xs-12" name="descripcion" placeholder="Ingresar Descripcion" required="">
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                          <label>Estado </label><br/>
+                          <label>
+                            <input type="checkbox" class="js-switch" checked name="estado" /> Activo
+                          </label>
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                            <div class="ln_solid"></div>
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                          <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                           
+                            
+                          </div>
+                      </div>
+                    </form>                                     
+                </div>
+
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
+
+
